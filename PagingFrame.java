4dc0a1bd39e65242274add07e3d9a7ecc5d467fa
heiner15 <heiner@mail.gvsu.edu>
2016-11-04 21:19:07 -0400

    Initial commit

---
+++ PagingFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+class PagingFrame extends JFrame {
+	public PagingFrame() {
+		setTitle("Paging Tutor");
+		setSize(200,300);
+	}
+
+	public static void main(String[] args) {
+		JFrame f = new PagingFrame();
+		f.show();
+	}
+}
