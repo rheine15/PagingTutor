e55574566394c94796d38993af420ae12259c787
heiner15 <heiner@mail.gvsu.edu>
2016-11-11 11:21:14 -0500

    added empty files

---
+++ PagingTutorGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+class PagingTutorGUI extends JFrame {
+	public PagingTutorGUI() {
+		setTitle("Paging Tutor");
+		setSize(200,300);
+	}
+
+	public static void main(String[] args) {
+		JFrame f = new PagingTutorGUI();
+		f.show();
+	}
+}
